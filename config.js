d646349fd2ce7daf881020b788504205c2a23ca8
SenujaWa <136876760+SenujaWa@users.noreply.github.com>
2023-06-18 10:34:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94713516807', 'Owner', true],
+  ['94713516807', 'TuTu BoT', true], 
   [''] 
 ] //Number of owners
 
